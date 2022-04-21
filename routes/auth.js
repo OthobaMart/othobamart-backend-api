d27e9d76cdc862f3b62e209584be35d87530a844
Nafiz Anam <nafiz.anam002@gmail.com>
2022-04-22 01:39:07 +0600

    few bug fix

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,54 @@ const jwt = require("jsonwebtoken");
 const userSchema = require("../schemas/userSchema");
 const User = new mongoose.model("User", userSchema);
 
+//login
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({
+            user_name: req.body.user_name,
+        });
+        // console.log(user);
+        !user &&
+            res.status(500).json({
+                status: 1,
+                error: "Wrong User/Password!",
+            });
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+        );
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+        const inputPassword = req.body.password;
+        originalPassword !== inputPassword &&
+            res.status(500).json({
+                status: 1,
+                error: "Wrong User/Password!",
+            });
+        const accessToken = await jwt.sign(
+            {
+                id: user._id,
+                isAdmin: user.isAdmin,
+                isSuperAdmin: user.isSuperAdmin,
+                isCustomer: user.isCustomer,
+                isVendor: user.isVendor,
+                vendor_status: user.vendor_status,
+                shop_id: user?.shop,
+            },
+            process.env.JWT_SEC,
+            { expiresIn: "1d" }
+        );
+        const { password, ...others } = user._doc;
+        res.status(200).json({ status: 0, ...others, accessToken });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            status: 1,
+            res: err,
+            error: "There was a server side error!",
+        });
+    }
+});
+
 // customer register
 router.post("/register", async (req, res) => {
     const newUser = new User({
@@ -60,54 +108,6 @@ router.post("/register/vendor", async (req, res) => {
             });
         }
     });
-});
-
-//login
-router.post("/login", async (req, res) => {
-    try {
-        const user = await User.findOne({
-            user_name: req.body.user_name,
-        });
-        // console.log(user);
-        !user &&
-            res.status(500).json({
-                status: 1,
-                error: "Wrong User/Password!",
-            });
-        const hashedPassword = CryptoJS.AES.decrypt(
-            user.password,
-            process.env.PASS_SEC
-        );
-        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-        const inputPassword = req.body.password;
-        originalPassword !== inputPassword &&
-            res.status(500).json({
-                status: 1,
-                error: "Wrong User/Password!",
-            });
-        const accessToken = jwt.sign(
-            {
-                id: user._id,
-                isAdmin: user.isAdmin,
-                isSuperAdmin: user.isSuperAdmin,
-                isCustomer: user.isCustomer,
-                isVendor: user.isVendor,
-                vendor_status: user.vendor_status,
-                shop_id: user?.shop,
-            },
-            process.env.JWT_SEC,
-            { expiresIn: "1d" }
-        );
-        const { password, ...others } = user._doc;
-        res.status(200).json({ status: 0, ...others, accessToken });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            status: 1,
-            res: err,
-            error: "There was a server side error!",
-        });
-    }
 });
 
 module.exports = router;
